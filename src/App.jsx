02cbed94555942916f37da1bb57e2f4122f1e947
Evgeny <evgeny.gutz@yandex.ru>
2025-09-01 09:46:24 +0300

    feature: добавить компоненты Button, Title, Paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css';
-import { Title } from './components/Title/Title';
+import Title from './components/Title/Title';
+import Button from './components/Button/Button';
+import Paragraph from './components/Paragraph/Paragraph';
 
 function App() {
-  return (
-    <Title type='h3'>Поиск</Title>
-  );
+	return (
+		<>
+			<Title type='h3'>Поиск</Title>
+			<Button>Я кнопка</Button>
+			<Paragraph>Lorem ipsum dolor sit amet consectetur adipisicing \n
+        elit. Nesciunt consectetur inventore soluta asperiores pariatur, odit quas repellat quia corporis? Aut officia dolores commodi quisquam, blanditiis provident esse enim nobis recusandae!</Paragraph>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/Paragraph/Paragraph.jsx
@@ -0,0 +1,9 @@
+import './Paragraph.css';
+
+const Paragraph = ({children}) => {
+	return (
+		<p className='paragraph'>{children}</p>
+	);
+};
+
+export default Paragraph;
