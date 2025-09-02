ad0088653375d313b95f27faf6b92d67c9bbddee
Evgeny <evgeny.gutz@yandex.ru>
2025-09-02 09:58:17 +0300

    fix: исправил нэйминг

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
-import stryles from './Button.module.css';
+import styles from './Button.module.css';
 
 const Button = ({children, onClick}) => {
 	return (
-		<button className={stryles.button} onClick={onClick}>{children}</button>
+		<button className={styles.button} onClick={onClick}>{children}</button>
 	);
 };
 
